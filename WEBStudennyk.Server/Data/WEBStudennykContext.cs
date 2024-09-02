6bf293c37bb81664666a01de024671a0ebcebf6e
Ura3535 <urakovalchuk3535@gmail.com>
2024-09-02 15:57:13 +0300

    Apd DBcontext

--- WEBStudennyk.Server/Data/WEBStudennykContext.cs
+++ WEBStudennyk.Server/Data/WEBStudennykContext.cs
@@ -14,17 +14,6 @@ namespace WEBStudennyk.Server.Data
             : base(options)
         {
         }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            //builder.Entity<User>()
-            //    .HasOne(u => u.Photo)
-            //    .WithOne()
-            //    .HasForeignKey<User>(u => u.PhotoUrlId);
-
-            builder.ApplyConfiguration(new RoleConfiguration());
-        }
 
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Group> Groups { get; set; }
@@ -35,6 +24,113 @@ namespace WEBStudennyk.Server.Data
         public DbSet<Subject> Subjects { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Group>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+                entity.HasMany(e => e.Students)
+                    .WithOne(e => e.Group)
+                    .HasForeignKey(e => e.GroupId)
+                    .OnDelete(DeleteBehavior.ClientCascade)
+                    .IsRequired();
+                entity.HasMany(e => e.SubGroups)
+                    .WithOne(e => e.Group)
+                    .HasForeignKey(e => e.GroupId)
+                    .OnDelete(DeleteBehavior.ClientCascade)
+                    .IsRequired();
+                entity.HasMany(e => e.Subjects)
+                    .WithOne(e => e.Group)
+                    .HasForeignKey(e => e.GroupId)
+                    .OnDelete(DeleteBehavior.ClientCascade)
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<Schedule>(entity =>
+            {
+                entity.Property(e => e.ClassesType)
+                    .IsRequired();
+                entity.Property(e => e.ClassesNumber)
+                    .IsRequired();
+                entity.Property(e => e.Period)
+                    .IsRequired();
+                entity.Property(e => e.DayOfWeek)
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+                entity.HasMany(e => e.StudentSubGroups)
+                    .WithOne(e => e.Student)
+                    .HasForeignKey(e => e.StudentId)
+                    .OnDelete(DeleteBehavior.ClientCascade)
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<SubGroup>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+                entity.HasMany(e => e.StudentSubGroups)
+                    .WithOne(e => e.SubGroup)
+                    .HasForeignKey(e => e.SubGroupId)
+                    .OnDelete(DeleteBehavior.ClientCascade)
+                    .IsRequired();
+                entity.HasMany(e => e.Schedules)
+                    .WithOne(e => e.SubGroup)
+                    .HasForeignKey(e => e.SubGroupId)
+                    .OnDelete(DeleteBehavior.ClientCascade)
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<Subject>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+                entity.Property(e => e.FinalControl)
+                    .IsRequired();
+                entity.Property(e => e.Description)
+                    .HasColumnType("text")
+                    .IsRequired(false);
+                entity.HasMany(e => e.Schedules)
+                    .WithOne(e => e.Subject)
+                    .HasForeignKey(e => e.SubjectId)
+                    .OnDelete(DeleteBehavior.ClientCascade)
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+                entity.Property(e => e.Info)
+                    .HasColumnType("text")
+                    .IsRequired(false);
+                entity.HasMany(e => e.CuratorGroups)
+                    .WithOne(e => e.Curator)
+                    .HasForeignKey(e => e.CuratorId)
+                    .OnDelete(DeleteBehavior.SetNull);
+                entity.HasMany(e => e.TeacherSubGroups)
+                    .WithOne(e => e.Teacher)
+                    .HasForeignKey(e => e.TeacherId)
+                    .OnDelete(DeleteBehavior.SetNull);
+                entity.HasMany(e => e.TeacherSchedules)
+                    .WithOne(e => e.Teacher)
+                    .HasForeignKey(e => e.TeacherId)
+                    .OnDelete(DeleteBehavior.ClientCascade)
+                    .IsRequired();
+            });
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        }
     }
 
 }
